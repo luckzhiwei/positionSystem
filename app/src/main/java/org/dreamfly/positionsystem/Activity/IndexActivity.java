214df21c9edd49ba5f0ce3421b96f19cc0206eea
lzw <lzw00000@qq.com>
2015-01-12 16:50:04 +0800

    测试GIT是否正常

--- app/src/main/java/org/dreamfly/positionsystem/Activity/IndexActivity.java
+++ app/src/main/java/org/dreamfly/positionsystem/Activity/IndexActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import org.dreamfly.positionsystem.R;
 
@@ -19,6 +20,7 @@ public class IndexActivity extends ActionBarActivity {
      */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.index_layout);
     }
 
