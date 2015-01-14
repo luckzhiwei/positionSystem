abc4edf4d5522b158c4801c5a5c5d89162dcd9e9
lzw <lzw00000@qq.com>
2015-01-14 22:15:59 +0800

    加入注册界面，并且把代码的命名都规范了,XML没有写注释

--- app/src/main/java/org/dreamfly/positionsystem/Activity/LoginActivity.java
+++ app/src/main/java/org/dreamfly/positionsystem/Activity/LoginActivity.java
@@ -1,22 +1,28 @@
 package org.dreamfly.positionsystem.Activity;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.dreamfly.positionsystem.R;
+
 import android.app.Activity;
+import android.widget.Toast;
+
 /**
  * Created by zhengyl on 15-1-13.
  * 登陆界面Activity类
  */
-public class LoginActivity extends Activity{
+public class LoginActivity extends Activity {
 
     private TextView txtLoginactivityRegister;
-    private  Button btnLoginactivityLogin;
+    private Button btnLoginactivityLogin;
     private EditText edittextLoginactivityUsername;
     private EditText editextLoginactivityPassword;
 
@@ -24,22 +30,43 @@ public class LoginActivity extends Activity{
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.login_layout);
-
+        this.initila();
+        ;
     }
 
     /**
      * @deprecated 初始化函数
      * 对组件进行视图的ID绑定
      */
-    private void initila(){
-        this.txtLoginactivityRegister=(TextView)
+    private void initila() {
+        this.txtLoginactivityRegister = (TextView)
                 findViewById(R.id.txt_loginactivity_register);
-        this.btnLoginactivityLogin=(Button)
+        this.btnLoginactivityLogin = (Button)
                 findViewById(R.id.btn_loginactivity_login);
-        this.edittextLoginactivityUsername=(EditText)
+        this.edittextLoginactivityUsername = (EditText)
                 findViewById(R.id.edtext_loginactivity_username);
-        this.editextLoginactivityPassword=(EditText)
+        this.editextLoginactivityPassword = (EditText)
                 findViewById(R.id.edtext_loginactivity_password);
+        this.bindListener();
     }
+
+    /**
+     * @deprecated 注册组件的监听器
+     */
+    private void bindListener() {
+
+        //跳入注册界面的事件监听
+        this.txtLoginactivityRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent registerIntent = new Intent();
+                registerIntent.setClass(getApplicationContext(), RegistActivity.class
+
+                );
+                startActivity(registerIntent);
+
+            }
+        });
+    }
+
 }
 
--- app/src/main/java/org/dreamfly/positionsystem/Activity/RegistActivity.java
+++ app/src/main/java/org/dreamfly/positionsystem/Activity/RegistActivity.java
@@ -1,10 +1,42 @@
 package org.dreamfly.positionsystem.Activity;
 
+import android.os.Bundle;
+
+import org.dreamfly.positionsystem.R;
+
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.Activity;
 
 /**
  * Created by zhengyl on 15-1-13.
  * 注册界面Activity类
  */
-public class RegistActivity extends ActionBarActivity {
+public class RegistActivity extends Activity {
+
+    private EditText editRegisterActivityUsername;
+    private EditText editRegisterActivityPassword;
+    private Button btnRegisterActivityRegister;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(R.layout.regsiter_layout);
+        this.initial();
+    }
+
+    /**
+     * 初始化绑定组件ID
+     */
+    private void initial() {
+        this.editRegisterActivityPassword =
+                (EditText) this.findViewById(R.id.editext_registeractivity_password);
+        this.editRegisterActivityUsername =
+                (EditText) this.findViewById(R.id.eidtext_registeractivity_username);
+        this.btnRegisterActivityRegister =
+                (Button) this.findViewById(R.id.btn_registeractivity_register);
+    }
+
 }
