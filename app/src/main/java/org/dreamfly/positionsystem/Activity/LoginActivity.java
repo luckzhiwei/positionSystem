2afafd990347303787373aa1fbbc66a9f87343e5
lzw <lzw00000@qq.com>
2015-01-14 19:26:40 +0800

    改变了注册界面上的某些不正确的命名规范

--- app/src/main/java/org/dreamfly/positionsystem/Activity/LoginActivity.java
+++ app/src/main/java/org/dreamfly/positionsystem/Activity/LoginActivity.java
@@ -2,32 +2,43 @@ package org.dreamfly.positionsystem.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.dreamfly.positionsystem.R;
-
+import android.app.Activity;
 /**
  * Created by zhengyl on 15-1-13.
  * 登陆界面Activity类
  */
-public class LoginActivity extends ActionBarActivity{
-    TextView txt_loginactivity_register;
-    Button btn_loginactivity_login;
-    EditText edtext_loginactivity_username ,edtext_loginactivity_password;
+public class LoginActivity extends Activity{
+
+    private TextView txtLoginactivityRegister;
+    private  Button btnLoginactivityLogin;
+    private EditText edittextLoginactivityUsername;
+    private EditText editextLoginactivityPassword;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.login_layout);
-        txt_loginactivity_register=(TextView)
-                findViewById(R.id.txt_loginactivity_register);
 
-        btn_loginactivity_login=(Button)
+    }
+
+    /**
+     * @deprecated 初始化函数
+     * 对组件进行视图的ID绑定
+     */
+    private void initila(){
+        this.txtLoginactivityRegister=(TextView)
+                findViewById(R.id.txt_loginactivity_register);
+        this.btnLoginactivityLogin=(Button)
                 findViewById(R.id.btn_loginactivity_login);
-        edtext_loginactivity_username=(EditText)
+        this.edittextLoginactivityUsername=(EditText)
                 findViewById(R.id.edtext_loginactivity_username);
-        edtext_loginactivity_password=(EditText)
+        this.editextLoginactivityPassword=(EditText)
                 findViewById(R.id.edtext_loginactivity_password);
     }
 }
