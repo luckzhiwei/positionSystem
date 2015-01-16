4d811097e68fa698353f5b4945f0cf216c6adab6
lzw <lzw00000@qq.com>
2015-01-16 14:13:46 +0800

    基本将下拉刷新的listview写好了 -lzw

--- app/src/main/java/org/dreamfly/positionsystem/Custom/DefineListView.java
+++ app/src/main/java/org/dreamfly/positionsystem/Custom/DefineListView.java
@@ -2,6 +2,7 @@ package org.dreamfly.positionsystem.Custom;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -15,8 +16,12 @@ public class DefineListView extends ListView {
 
          private Context mContext;
          private DefineListViewHeader mListViewHeader;
+         private int   userTouchDistance;//用户实际划过的距离
 
          private float touchY;
+
+         private static final int  MINDISTANCE=250;
+         private static final int  MAXDISTATNCE=600;
 
          public DefineListView(Context context)
          {
@@ -36,14 +41,24 @@ public class DefineListView extends ListView {
                this.initial(context);
          }
 
+            /**
+             * 初始化参数,主要把listview顶部的高度设置为0
+             * @param mContext
+             */
          private void initial(Context mContext)
          {
              this.mContext=mContext;
              this.mListViewHeader=new DefineListViewHeader(this.mContext);
              this.addHeaderView(this.mListViewHeader);
              this.touchY=0.0f;
+             this.userTouchDistance=0;
          }
 
+        /**
+         * 为listivew设置事件监听
+         * @param event
+         * @return
+         */
          public boolean onTouchEvent(MotionEvent event)
          {
               if(event.getAction()==MotionEvent.ACTION_DOWN)
@@ -52,37 +67,58 @@ public class DefineListView extends ListView {
               }
               else if(event.getAction()==MotionEvent.ACTION_MOVE)
               {
-                   float detalY=event.getRawY()-this.touchY;
-                   this.touchY=event.getRawY();
+                  float detalY=event.getRawY()-this.touchY;
+                  this.touchY=event.getRawY();
                   this.dynSetHeadViewHeight(this.calDistance(detalY));
               }
               else if(event.getAction()==MotionEvent.ACTION_UP)
               {
-                    if(this.getFirstVisiblePosition()==0)
+                    if(this.getFirstVisiblePosition()==0
+                            &&this.userTouchDistance>MINDISTANCE)
                     {
-                          this.dynSetHeadViewHeight(250);
+                        this.dynSetHeadViewHeight(250);
+                        this.userTouchDistance=0;
                     }
+
+                    //用户划过的距离必须超过最小才行，对顶部设置一个合适的大小来显示
+                    else if(this.userTouchDistance<MINDISTANCE)
+                    {
+                        this.dynSetHeadViewHeight(0);
+                    }
+                    //如果用户实际划过的距离小于最小距离,那么listview的头部是不会显示的
+                    this.userTouchDistance=0;
+                    //每次划过之后都将用户的实际划过的距离清零
               }
 
               return(super.onTouchEvent(event));
          }
 
-
+        /**
+         * 动态设置设置listview头顶部的长度
+         * @param height
+         */
          private void dynSetHeadViewHeight(int height)
          {
                this.mListViewHeader.setDynHeight(height);
          }
 
+        /**
+         * 参数表示计算划长度过的距离,累加算法来计算下来的
+         * @param deltaY
+         * @return
+         */
          private int calDistance(float deltaY)
          {
               int realDistance=0;
               if(deltaY>0 && this.getFirstVisiblePosition()==0)
               {
                     realDistance=(int)deltaY+this.mListViewHeader.getCurrentHeight();
-                    if(realDistance>=300)
+                    this.userTouchDistance=realDistance;
+                    if(realDistance>=MAXDISTATNCE)
                     {
-                        realDistance=300;
+                        realDistance=MAXDISTATNCE;
                     }
+
               }
               return (realDistance);
          }
--- app/src/main/java/org/dreamfly/positionsystem/Custom/DefineListViewHeader.java
+++ app/src/main/java/org/dreamfly/positionsystem/Custom/DefineListViewHeader.java
@@ -66,7 +66,4 @@ public class DefineListViewHeader  extends LinearLayout{
 
 
 
-
-
-
 }
