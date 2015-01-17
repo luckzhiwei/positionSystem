14d9cef5c019821d7a5f33ee6132f05241e9b9f1
zyl123ok <zyl123ok@163.com>
2015-01-17 18:35:03 +0800

    zhengyl

--- app/src/main/java/org/dreamfly/positionsystem/Database/DataBase.java
+++ app/src/main/java/org/dreamfly/positionsystem/Database/DataBase.java
@@ -3,49 +3,70 @@ package org.dreamfly.positionsystem.Database;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
  * Created by zhengyl on 15-1-16.
  */
-public class DataBase {
+public class DataBase extends SQLiteOpenHelper{
     private static final String TAG="Position_System";
     private static final String DATABASE_NAME="positionsystem.db";
+    private final static int DATABASE_VERSION = 1;
+    private final static String TABLE_NAME = "items";
     SQLiteDatabase db;
+    private Cursor cursor;
     Context context;
     public DataBase(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
         this.context=context;
-        db=context.openOrCreateDatabase(DATABASE_NAME,0,null);
-        Log.v(TAG,"db path="+db.getPath());
+
     }
-    public void CreateTable_items(){
-        try{
-            db.execSQL("CREATE TABLE items("+"_id INTEGER primary key autoincrement,"+"name TEXT,"+
-           "subname TEXT,"+"position TEXT,"+"time TEXT"+");");
-            Log.v(TAG,"create table ok");
+    public void onCreate(SQLiteDatabase db){
+        try {
+            db.execSQL("CREATE TABLE items(" + "_id INTEGER primary key autoincrement," + "name TEXT," +
+                    "subname TEXT," + "position TEXT," + "time TEXT" + ");");
+            Log.v(TAG, "create table ok");
         }
         catch (Exception e){
             Log.v(TAG,"create table err");
         }
     }
-    public boolean items_save(String name,String  subname,String position,String time){
-        String sql="";
-        try {
-            sql="insert into items values(null,'"+name+"','"+subname+"','"+position+"','"+time+"')";
-            db.execSQL(sql);
-            Log.v("TAG","insert into table items ="+sql);
-            return true;
-        }
-        catch (Exception e){
-            Log.v("TAG","insert into table items err="+sql);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+    /**
+     * 判断表是否存在
+     *@param tableName 表名
+     *
+    public boolean tabbleIsExist(String tableName){
+        boolean result=false;
+        if(tableName ==null){
             return false;
         }
-    }
-    public Cursor getItems(){
-        Log.v("TAG","get items from database");
-        String[] colums=new String[]{"_id","name","subname","position","time"};
-        return db.query("items",colums,null,null,null,null,null);
-    }
+        try{
+            db=this.getReadableDatabase();
+            String sql="select count(*) as c from Sqlite_master  " +
+                    "where type ='table' and name ='\"+items.trim()+\"'";
+            cursor=db.rawQuery(sql,null);
+            if(cursor.moveToNext()){
+                int count=cursor.getInt(0);
+                if(count>0){result=true;}
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if(null != cursor && !cursor.isClosed()){
+                cursor.close() ;
+            }
+        }
+        return result;
+    }*/
 
 
 }
