01673600325f267ef55abb3acc6261cd9890758e
lzw <lzw00000@qq.com>
2015-01-17 21:20:01 +0800

    写好了关于用户缓存的本地文件工具类,并且暴露了接口函数

--- app/src/main/java/org/dreamfly/positionsystem/Utils/FileUitls.java
+++ app/src/main/java/org/dreamfly/positionsystem/Utils/FileUitls.java
@@ -1,11 +1,47 @@
 package org.dreamfly.positionsystem.Utils;
 
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
 /**
  * Created by liaozhiwei on 2015/1/12.
  * 关于文件的处理的工具类
  */
 public class FileUitls {
 
+    private static String DIRNAME = "/posSystem/";
+
+    private File mCacheFileDir;
+
+    /**
+     * 建立该APP的缓存文件夹
+     */
+    public FileUitls() {
+        Log.i("lzw", "file");
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            Log.i("lzw", "sd right");
+            this.mCacheFileDir = new File(android.os.Environment.getExternalStorageDirectory(), DIRNAME);
+            if (!this.mCacheFileDir.exists()) {
+                this.mCacheFileDir.mkdirs();
+                Log.i("lzw", "create");
+            }
+        } else {
+
+        }
+    }
+
+    public File getCacheFileDir() {
+        return (this.mCacheFileDir);
+    }
+
+    public void createFoler(String folerName) {
+        File createFolder = new File(this.mCacheFileDir, "/" + folerName + "/");
+        if (!createFolder.exists()) {
+            createFolder.mkdir();
+        }
+    }
 
 
 }
