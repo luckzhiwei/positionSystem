582a483a9c5b85df2aab22482d051e06e1420a56
lzw <lzw00000@qq.com>
2015-01-18 20:05:38 +0800

    自定义对话框已经写好

--- app/src/main/java/org/dreamfly/positionsystem/Activity/RegistActivity.java
+++ app/src/main/java/org/dreamfly/positionsystem/Activity/RegistActivity.java
@@ -2,13 +2,16 @@ package org.dreamfly.positionsystem.Activity;
 
 import android.os.Bundle;
 
+import org.dreamfly.positionsystem.Custom.DefineDialog;
 import org.dreamfly.positionsystem.R;
 
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.app.Activity;
+import android.widget.Toast;
 
 /**
  * Created by zhengyl on 15-1-13.
@@ -20,17 +23,26 @@ public class RegistActivity extends Activity {
     private EditText editRegisterActivityPassword;
     private Button btnRegisterActivityRegister;
 
+    private DefineDialog mDefineDialog;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.regsiter_layout);
         this.initial();
+
     }
 
     /**
      * 初始化绑定组件ID
      */
     private void initial() {
+
+        this.bindID();
+        this.bindListener();
+    }
+
+    private void bindID() {
         this.editRegisterActivityPassword =
                 (EditText) this.findViewById(R.id.editext_registeractivity_password);
         this.editRegisterActivityUsername =
@@ -39,4 +51,7 @@ public class RegistActivity extends Activity {
                 (Button) this.findViewById(R.id.btn_registeractivity_register);
     }
 
+    private void bindListener() {
+
+    }
 }
--- app/src/main/java/org/dreamfly/positionsystem/Custom/DefineDialog.java
+++ app/src/main/java/org/dreamfly/positionsystem/Custom/DefineDialog.java
@@ -2,11 +2,17 @@ package org.dreamfly.positionsystem.Custom;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.dreamfly.positionsystem.R;
 /**
  * Created by asus on 2015/1/17.
  */
@@ -15,9 +21,19 @@ public class DefineDialog {
 
            private Dialog  mDefDialog;
            private Context mContext;
-           private Display mDisplay;
+           private Display mDisplay;//获取屏幕相关信息的类
 
-           public DefineDialog(Context  mContext)
+
+           private TextView txtDefineDialogTitile;
+           private Button  btnDefineDialogPositive;
+           private Button  btnDefineDialogNegetive;
+
+        /**
+        * 自定义对话框的构造方法：这里一定要把activity.this传入作为上下文参数,
+         * 不能使用getAplicationContext()函数,因为是进程上下文
+        * @param mContext
+        */
+           public DefineDialog(Context mContext)
            {
                this.mContext=mContext;
                WindowManager winManager=
@@ -26,12 +42,98 @@ public class DefineDialog {
 
            }
 
-           public DefineDialog buiidler()
+            /**
+             * 初始化dialog的视图,加载自定义的layout文件
+             * @return (返回自身,链式调用)
+             */
+           public DefineDialog buiider()
            {
-//                 View dialogView= LayoutInflater.from(this.mContext)
-
-                 return this;
+                 View dialogView= LayoutInflater.from(this.mContext).inflate(R.layout.dialogdef_layout,null);
+                 this.bindID(dialogView);
+                 dialogView.setMinimumWidth(this.mDisplay.getWidth());
+                 this.mDefDialog=new Dialog(this.mContext,R.style.ActionSheetDialogStyle);
+                 this.mDefDialog.setContentView(dialogView);
+                 Window  dialogWindow=this.mDefDialog.getWindow();
+                 dialogWindow.setGravity(Gravity.CENTER);
+                 this.bindNegBtnListener();
+                 return (this);
            }
 
+        /**
+         * 绑定控件ID
+         * @param contentView
+         */
+           private void bindID(View contentView)
+           {
+                  this.txtDefineDialogTitile=(TextView)contentView.findViewById(R.id.txt_definedialog_title);
+                  this.btnDefineDialogPositive=(Button)contentView.findViewById(R.id.btn_dialogdefine_positive);
+                  this.btnDefineDialogNegetive=(Button)contentView.findViewById(R.id.btn_dialogdefine_negetive);
+           }
 
+           public DefineDialog setTitle(String dialgTitle)
+           {
+                  this.txtDefineDialogTitile.setText(dialgTitle);
+                  return (this);
+           }
+
+           public DefineDialog setPosBtnTxt(String posBtnTxt)
+           {
+                 this.btnDefineDialogPositive.setText(posBtnTxt);
+                 return(this);
+           }
+
+           public DefineDialog setNegBtnTxt(String ngeBtnTxt)
+           {
+                 this.btnDefineDialogNegetive.setText(ngeBtnTxt);
+                 return(this);
+           }
+
+           private void bindNegBtnListener()
+           {
+                  this.btnDefineDialogNegetive.setOnClickListener(new View.OnClickListener() {
+                      public void onClick(View view) {
+                         mDefDialog.dismiss();
+                      }
+                  });
+           }
+
+        /**
+         * 点击对话框区域之外后是否可以让对话框消失
+         * @param canceable
+         * @return
+         */
+           public DefineDialog setDefineDialogCanceable(boolean  canceable)
+           {
+                 this.mDefDialog.setCancelable(canceable);
+                 return(this);
+           }
+
+            /**
+             *设置确定按键按下后的事件
+             * @param defineClickListener
+             * @return
+             */
+           public DefineDialog setPosBtnClickListener(View.OnClickListener  defineClickListener)
+           {
+                 this.btnDefineDialogPositive.setOnClickListener(defineClickListener);
+                 return(this);
+           }
+
+            /**
+             *出现对话框
+             * @return
+             */
+           public DefineDialog show()
+           {
+                 this.mDefDialog.show();
+                 return(this);
+           }
+
+    /**
+     * 消失
+     */
+           public void dismiss()
+           {
+                 this.mDefDialog.dismiss();
+           }
 }
